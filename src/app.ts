1e19d71c251dccc5f877256990876295170eac5b
Hanafi6 <----GitHub>
2026-09-11 05:20:49 +0300

    fix: install cors and update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,25 @@
 import express from 'express';
-// import productsRouter from './routes/courses.routes.js';
+import cors from 'cors';
 
-import todosRouter from '@/routes/todos.routes.js';
-import tabsRout from '@/routes/tabs.routes.js';
-import { env } from '@/lib/env.js';
+import todosRouter from './routes/todos.routes.js';
+import tabsRout from './routes/tabs.routes.js';
+import { env } from './lib/env.js';
 
 export const app = express();
 
-const PORT = 3200;
-
+app.use(cors());
 app.use(express.json());
 
-// app.use('/api/products', productsRouter);
-
+// تعريف الـ Routes
 app.use('/api/tabs', tabsRout);
 app.use('/api/todos', todosRouter);
 
+// تشغيل الـ Server محلياً فقط لمنع Crash على Vercel Serverless
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = env.PORT || 3200;
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-
-});
 export default app;
